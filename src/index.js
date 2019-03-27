4bdab5d1480f4149e2271a39b500d453acd829c3
GilTorch <infotorch2014@gmail.com>
2019-03-27 10:08:15 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import manageTodo from './reducers/manageTodo';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
-let store = createStore(manageTodo);
+let store = createStore(manageTodo,window._REDUX_DEVTOOLS_EXTENSION && window._REDUX_DEVTOOLS_EXTENSION_());
 
 ReactDOM.render(
   <Provider store={store}>
